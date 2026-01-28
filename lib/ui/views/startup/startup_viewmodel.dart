df02cbaf0ae9d02a0cc097480cabe532e84d8a75
atandoc <48207698+tandocaileen@users.noreply.github.com>
2026-01-28 21:12:20 +0800

    fix: update navigation route in StartupViewModel from HomeView to DashboardView

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -4,6 +4,6 @@ import 'package:mm_inventory_web/app/app.router.dart';
 class StartupViewModel extends BaseViewModel {
   Future runStartupLogic() async {
     await Future.delayed(const Duration(seconds: 2));
-    stackedRouter.replace(const HomeViewRoute());
+    stackedRouter.replace(const DashboardViewRoute());
   }
 }
