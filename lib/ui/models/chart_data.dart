d2bbe7f1b51064881f704f61df65f8644af557be
atandoc <48207698+tandocaileen@users.noreply.github.com>
2026-01-28 16:50:47 +0800

    color

--- lib/ui/models/chart_data.dart
+++ lib/ui/models/chart_data.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 class ChartData {
-  ChartData(this.category, this.value);
+  ChartData(this.category, this.value, {this.color});
   final String category;
   final double value;
+  final Color? color;
 }
